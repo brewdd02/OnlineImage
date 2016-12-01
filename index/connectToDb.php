8f6db1c9778e675317ae99162f2babddb83d0a08
John Doe <jared@perrycode.com>
2016-12-01 19:10:48 +0000

    add getUsername() helper func

--- index/connectToDb.php
+++ index/connectToDb.php
@@ -6,16 +6,30 @@
 		$dbUsername = 'root';
 		$dbPassword = '';
 		$dbName = 'boxdrop';
-		
+
 		//connect with the database
 		$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-		
+
 		if($conn->connect_errno)
 		{
 			echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 			return;
 		}
-		
+
 		return $conn;
+	}
+
+	function getUsername()
+	{
+		$uid = $_SESSION["uid"];
+		$db = connect();
+		$stGetUser = $db->prepare("SELECT username FROM users WHERE id = (?) LIMIT 1") or die(mysqli_error($db));
+		$stGetUser->bind_param("i", $uid) or die(mysqli_error($db));
+		$stGetUser->execute() or die(mysqli_error($db));
+		$stGetUser->bind_result($rUsername) or die(mysqli_error($db));
+		if (!$stGetUser->fetch()) {
+			return 'nobody';
+		}
+		return $rUsername;
 	}
 ?>
